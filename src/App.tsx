c43f87ee9791fb3d1628545048df66fac2d5b4b6
Masaya Nakamura <mashabow@shiromoji.net>
2020-02-11 23:57:35 +0900

    Enable to rotate the image

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState, useCallback } from 'react';
 import Cropper from 'react-cropper';
 import 'cropperjs/dist/cropper.css';
 import './App.css';
 
 const App = () => {
-  const cropper = useRef();
+  const cropper = useRef<Cropper | null>(null);
   const previewClassName = 'Preview';
+
+  const [angle, setAngle] = useState(0);
+  const updateAngle = useCallback(event => {
+    setAngle(event.target.value);
+    if (cropper.current) {
+      cropper.current.rotateTo(event.target.value);
+    }
+  }, []);
 
   return (
     <div className="App">
       <Cropper
         className="Cropper"
-        ref={cropper.current}
+        ref={c => {
+          cropper.current = c;
+        }}
         src="/daiji001.jpg"
         // Cropper.js options
         preview={`.${previewClassName}`}
@@ -20,6 +30,16 @@ const App = () => {
       />
       <div className="Sidebar">
         <div className={previewClassName} />
+        <input
+          type="range"
+          min="-10"
+          max="10"
+          step="0.1"
+          value={angle}
+          onInput={updateAngle}
+          onChange={updateAngle}
+        />
+        {angle}
       </div>
     </div>
   );
