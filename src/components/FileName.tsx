6d7cb533f8a78705ac6286f3d623eb37e550bf99
Masaya Nakamura <mashabow@shiromoji.net>
2020-02-15 22:54:18 +0900

    Confirm to close a source image

--- src/components/FileName.tsx
+++ src/components/FileName.tsx
@@ -1,4 +1,4 @@
-import { Icon } from 'antd';
+import { Icon, Modal } from 'antd';
 import React, { useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -7,16 +7,35 @@ import { close } from '../features/sourceImage';
 
 import styles from './FileName.module.css';
 
+const { confirm } = Modal;
+
 const FileName = () => {
   const dispatch = useDispatch();
   const sourceImage = useSelector(state => state.sourceImage);
 
-  const onClick = useCallback(() => {
+  const closeSourceImage = useCallback(() => {
     if (!sourceImage.url) return;
     URL.revokeObjectURL(sourceImage.url);
     dispatch(close());
     dispatch(clearCrops());
   }, [dispatch, sourceImage.url]);
+
+  const hasCrop = useSelector(state => state.crops.length > 0);
+  const onClick = useCallback(
+    () =>
+      hasCrop
+        ? confirm({
+            title: '画像を閉じてもよろしいですか？',
+            content: '切り抜きデータは破棄されます。',
+            okType: 'danger',
+            onOk: closeSourceImage,
+            okText: 'OK',
+            cancelText: 'キャンセル',
+            centered: true,
+          })
+        : closeSourceImage(),
+    [closeSourceImage, hasCrop],
+  );
 
   return (
     <div className={styles.FileName} onClick={onClick}>
