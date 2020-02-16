8c96282a0fa8c0d8d2126ad38ee7ebbfe6ca3b26
Masaya Nakamura <mashabow@shiromoji.net>
2020-02-16 20:25:55 +0900

    Use early return

--- src/hooks.ts
+++ src/hooks.ts
@@ -11,36 +11,35 @@ export const useEnterToCrop = (
   const handler = useCallback(
     (event: KeyboardEvent) => {
       const cropper = cropperRef.current;
-      if (cropper && event.key === 'Enter') {
-        const { x, y, width, height, rotate } = cropper.getData();
+      if (!(cropper && event.key === 'Enter')) return;
+      const { x, y, width, height, rotate } = cropper.getData();
 
-        // 本来であれば cropper.getCropedCanvas() で画像データを直に取得したいところだが、
-        // soruceImage のサイズが大きすぎると、NS_ERROR_FAILURE というエラーが出るなどして、
-        // 取得に失敗することがある。
-        // そこで、サムネイルと同じ大きさのダミーのプレビュー thumbnailPreviewClassName を用意して、
-        // crop 時にそのスタイルを取得し、サムネイル表示に利用するようにする。
-        const divStyle = document.querySelector<HTMLDivElement>(
-          `.${thumbnailPreviewClassName}`,
-        )!.style;
-        const imgStyle = document.querySelector<HTMLImageElement>(
-          `.${thumbnailPreviewClassName}>img`,
-        )!.style;
+      // 本来であれば cropper.getCropedCanvas() で画像データを直に取得したいところだが、
+      // soruceImage のサイズが大きすぎると、NS_ERROR_FAILURE というエラーが出るなどして、
+      // 取得に失敗することがある。
+      // そこで、サムネイルと同じ大きさのダミーのプレビュー thumbnailPreviewClassName を用意して、
+      // crop 時にそのスタイルを取得し、サムネイル表示に利用するようにする。
+      const divStyle = document.querySelector<HTMLDivElement>(
+        `.${thumbnailPreviewClassName}`,
+      )!.style;
+      const imgStyle = document.querySelector<HTMLImageElement>(
+        `.${thumbnailPreviewClassName}>img`,
+      )!.style;
 
-        dispatch(
-          addCrop({
-            params: { x, y, width, height, rotate },
-            divStyle: {
-              width: divStyle.width,
-              height: divStyle.height,
-            },
-            imgStyle: {
-              width: imgStyle.width,
-              height: imgStyle.height,
-              transform: imgStyle.transform,
-            },
-          }),
-        );
-      }
+      dispatch(
+        addCrop({
+          params: { x, y, width, height, rotate },
+          divStyle: {
+            width: divStyle.width,
+            height: divStyle.height,
+          },
+          imgStyle: {
+            width: imgStyle.width,
+            height: imgStyle.height,
+            transform: imgStyle.transform,
+          },
+        }),
+      );
     },
     [cropperRef, dispatch, thumbnailPreviewClassName],
   );
